0d8c9da1c8f50f68af301743499cf6d988ee49c3
69oo69 <53282201+69oo69@users.noreply.github.com>
2022-11-17 05:56:16 -0500

    Still Stuck....

---
+++ Models/Items.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06_JSON_Serialization_Deserialization.Models
+{
+    public class Items
+    {
+
+    }
+}
