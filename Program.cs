a44f5b4840267b8444ccdab8b5d183d5b0ca1e08
69oo69 <53282201+69oo69@users.noreply.github.com>
2022-11-17 00:23:44 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_06_JSON_Serialization_Deserialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
