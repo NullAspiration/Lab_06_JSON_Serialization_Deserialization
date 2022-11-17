b420b81b4f50359acca33a9c1e3ff52b6c101400
69oo69 <53282201+69oo69@users.noreply.github.com>
2022-11-17 05:40:18 -0500

    Stuck...

---
+++ Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06_JSON_Serialization_Deserialization.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Selflink { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public string Description { get; set; }
+
+        public Book(string id, string selfLink, string title, string authors, string description)
+        {
+     
+            Id = id;
+            Selflink = selfLink;
+            Title = title;
+            Authors = authors;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            string itemString = "";
+            itemString += $"ID: {Id}\n";
+            itemString += $"SelfLink: {Selflink}\n";
+            itemString += $"Title: {Title}\n";
+            itemString += $"Authors: {Authors}\n";
+            itemString += $"Description: {Description}\n";
+
+            return itemString;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Lab_06_JSON_Serialization_Deserialization
+﻿using Lab_06_JSON_Serialization_Deserialization.Models;
+using System.Net;
+using System.Text.Json;
+using ConsoleTables;
+
+namespace Lab_06_JSON_Serialization_Deserialization
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Creates Directory Variable Fot JSON
+            var root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+            //Points to Data
+            var filePath = root + $"{Path.DirectorySeparatorChar}Data{Path.DirectorySeparatorChar}Space Exploration.json";
+            var dataPath = root + $"{Path.DirectorySeparatorChar}Data";
+
+            //Serialization options (lazy no capitalization)
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            //Reads all .json data
+            foreach (string fileName in Directory.GetFiles(dataPath))
+            {
+                string jsonString = string.Empty;
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    jsonString = sr.ReadToEnd();
+                }
+                //just showing it reads the data folder and all contents
+                Console.WriteLine(jsonString);
+
+                var books = JsonSerializer.Deserialize<Book>(jsonString, options);
+
+                Console.WriteLine(books);
+            }
         }
     }
 }
